51cf4ec49548031fa4985617802dfc3e70affde0
Johan Friedner <j.friedner@gmail.com>
2021-01-14 14:58:58 +0100

    style: install Material-UI and style splash & PIN screens

--- client/src/components/Pin/Pin.jsx
+++ client/src/components/Pin/Pin.jsx
@@ -1,30 +1,108 @@
 
+
+
 import './Pin.css';
 import { useState } from 'react';
+
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 
 function Pin (props) {
 
   const [ pin, setPin ]= useState('');
+  const classes = useStyles();
 
   return (
-    <>
-    Pin<br/>
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        if (pin === '') return;
-        props.authUser(pin);
-        setPin('');
-      }}>
-        <input value={pin} onChange={(e) => {
-          setPin(e.target.value);
-        }
-        } type="text"/>
-        <button type="submit">Submit</button>
-      </form>
-    </>
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Enter your 4-digit PIN
+          </Typography>
+          <form className={classes.form} noValidate onSubmit={(e) => {
+            e.preventDefault();
+            if (pin === '') return;
+            props.authUser(pin);
+            setPin('');
+          }}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="PIN"
+              label="PIN"
+              type="password"
+              id="PIN"
+              onChange={(e) => {setPin(e.target.value);}}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Submit
+            </Button>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
   );
 }
 
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://images.unsplash.com/photo-1599880917812-35b7cde34597?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixlib=rb-1.2.1&q=80&w=1600)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(20, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 export default Pin;
+
+
+
+
+
 
--- client/src/components/Splashscreen/Splashscreen.jsx
+++ client/src/components/Splashscreen/Splashscreen.jsx
@@ -1,14 +1,58 @@
 
 import './Splashscreen.css';
 
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+
 function Splashscreen () {
+
+  const classes = useStyles();
+
   return (
-    <>
-    Splashscreen<br/>
-      <a href="/auth">Login</a>
-    </>
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={12} sm={12} md={12} className={classes.image} />
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        className={classes.submit}
+        onClick={() => {window.location.replace('/auth');}}
+      >
+              Press to begin
+      </Button>
+    </Grid>
   );
 }
+
+
+// Component style
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://images.unsplash.com/photo-1599880917812-35b7cde34597?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixlib=rb-1.2.1&q=80&w=1600)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  submit: {
+    margin: theme.spacing(3, 2, 2),
+    height: '10vh',
+    width: '20vw',
+    bottom: '0',
+    right: '0',
+    position: 'absolute',
+  },
+}));
 
 export default Splashscreen;
 
